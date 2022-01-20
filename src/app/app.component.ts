5568aaff7b87a376121c4331b7018e37b6b05eee
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-01-20 15:01:26 -0300

    Estado del servidor de sockets desde el cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'basico';
+export class AppComponent implements OnInit {
+
+  constructor(public wsService: WebsocketService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
 }

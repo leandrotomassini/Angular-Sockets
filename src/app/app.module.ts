dc4179612e2ba3775a5b8c5a44f4ef75c4ca8cbc
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-01-20 15:20:55 -0300

    Detectar la desconexion de un usuario - Servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
+import { FooterComponent } from './components/footer/footer.component';
 
 const config: SocketIoConfig = { 
   url: environment.wsUrl,
@@ -15,7 +16,8 @@ const config: SocketIoConfig = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

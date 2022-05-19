c96dd41dbd7e48ff7b0a11378037a851bad4b404
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-05-19 19:27:01 -0300

    Chat con diseño azul

--- angularSockets/src/app/home/home.page.ts
+++ angularSockets/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 import { ChatService } from '../services/chat.service';
@@ -11,6 +12,8 @@ import { WebsocketService } from '../services/websocket.service';
 })
 export class HomePage implements OnInit, OnDestroy {
 
+  @ViewChild(IonContent) content: IonContent
+
   texto = '';
   mensajesSubscription: Subscription;
   mensajes: any[] = [];
@@ -21,15 +24,13 @@ export class HomePage implements OnInit, OnDestroy {
 
   ngOnInit(): void {
 
-    this.elemento = document.getElementById('chat-mensajes');
+    setInterval(() => {
+      this.content.scrollToBottom(400);
+    }, 200);
 
     this.mensajesSubscription = this.chatService.getMessages().subscribe(msg => {
 
       this.mensajes.push(msg);
-
-      setTimeout(() => {
-        this.elemento.scrollTop = this.elemento.scrollHeight;
-      }, 50);
 
     });
   }
@@ -42,8 +43,13 @@ export class HomePage implements OnInit, OnDestroy {
     if (this.texto.trim().length === 0) {
       return;
     }
+
     this.chatService.sendMessage(this.texto);
     this.texto = '';
+
+    setTimeout(() => {
+      this.content.scrollToBottom(400);
+    });
   }
 
 }
